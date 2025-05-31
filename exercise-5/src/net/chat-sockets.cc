3a7248ebe0fa15122fbc3ba4f75594b83a710aaf
Sushil Raaja U <user@ip-172-31-30-215.ap-south-1.compute.internal>
2025-05-31 06:40:16 +0000

    Fixed chat-sockets.cc class function definitions

--- exercise-5/src/net/chat-sockets.cc
+++ exercise-5/src/net/chat-sockets.cc
@@ -1,24 +1,22 @@
 #include "chat-sockets.h"
 
-class SocketClass {
-public:
-  template <typename T, typename S> void check_error(T test, S error_message) {
-    if (test) {
-      std::cerr << error_message << "\n";
-      throw std::runtime_error(error_message);
-    }
+template <typename T, typename S> 
+void SocketClass::check_error(T test, S error_message) {
+  if (test) {
+    std::cerr << error_message << "\n";
+    throw std::runtime_error(error_message);
   }
+}
 
-  int create_socket(){
-    int sock = socket(AF_INET, SOCK_STREAM, 0);
-    check_error(sock < 0, "Socket creation error\n");
-    return sock;
-  }
+int SocketClass::create_socket(){
+  int sock = socket(AF_INET, SOCK_STREAM, 0);
+  check_error(sock < 0, "Socket creation error\n");
+  return sock;
+}
 
-  sockaddr_in create_address(int port) {
-    sockaddr_in address;
-    address.sin_family = AF_INET;
-    address.sin_port = htons(port);
-    return address;
-  }
-};
+sockaddr_in SocketClass::create_address(int port) {
+  sockaddr_in address;
+  address.sin_family = AF_INET;
+  address.sin_port = htons(port);
+  return address;
+}
