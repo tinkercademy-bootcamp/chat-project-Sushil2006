fb0d32e791178fff43836cc34e41818adcf88fa5
Sushil Raaja U <user@ip-172-31-30-215.ap-south-1.compute.internal>
2025-05-31 06:04:11 +0000

    Created SocketClass

--- exercise-5/src/net/chat-sockets.cc
+++ exercise-5/src/net/chat-sockets.cc
@@ -1,14 +1,23 @@
 #include "chat-sockets.h"
 
-int tt::chat::net::create_socket() {
-  int sock = socket(AF_INET, SOCK_STREAM, 0);
-  tt::chat::check_error(sock < 0, "Socket creation error\n");
-  return sock;
-}
+class SocketClass {
+  template <typename T, typename S> void check_error(T test, S error_message) {
+    if (test) {
+      std::cerr << error_message << "\n";
+      throw std::runtime_error(error_message);
+    }
+  }
 
-sockaddr_in tt::chat::net::create_address(int port) {
-  sockaddr_in address;
-  address.sin_family = AF_INET;
-  address.sin_port = htons(port);
-  return address;
-}
+  int create_socket(){
+    int sock = socket(AF_INET, SOCK_STREAM, 0);
+    check_error(sock < 0, "Socket creation error\n");
+    return sock;
+  }
+
+  sockaddr_in create_address(int port) {
+    sockaddr_in address;
+    address.sin_family = AF_INET;
+    address.sin_port = htons(port);
+    return address;
+  }
+};
